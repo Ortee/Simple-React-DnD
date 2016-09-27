ce54d79adf648ab16fdfec79ea6caa533d91d54a
Mateusz <kontakt@mateuszoracz.pl>
2016-09-27 14:09:30 +0200

    refactore Event

--- src/routes/Home/components/Event.js
+++ src/routes/Home/components/Event.js
@@ -12,22 +12,22 @@ export default class Event extends React.Component {
 
   render() {
     let topMargin;
-    if(this.props.hours === "08:00 - 09:00") {
-      topMargin = 0;
-    } else if(this.props.hours === "09:00 - 10:00") {
-      topMargin = 131;
-    } else if(this.props.hours === "10:00 - 11:00") {
-      topMargin = 262;
-    } else if(this.props.hours === "11:00 - 12:00") {
-      topMargin = 393;
-    } else if(this.props.hours === "12:00 - 13:00") {
-      topMargin = 524;
+    let event = {
+      current: "",
+      hours: this.props.hours,
+      description: [ "08:00 - 09:00", "09:00 - 10:00", "10:00 - 11:00", "11:00 - 12:00", "12:00 - 13:00" ],
+      topMargin : [ 0, 131, 262, 393, 524 ]
     }
+    event.description.map(function(item, index) {
+      if(event.hours === item) {
+        event.current = event.topMargin[index];
+      }
+    })
     return(
             <div  onDragStart={this.onDragStart.bind(this)}
                   className="event q4 past"
                   draggable="true"
-                  style={{top: topMargin+'px'}}>
+                  style={{top: event.current+'px'}}>
               <p className="hours">{this.props.hours}</p>
               <p className="description">{this.props.description}</p>
               <span className="icon"></span>
